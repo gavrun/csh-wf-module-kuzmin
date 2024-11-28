f2854a0356c4618eb2f05082e9255475b62f98d2
gavrun <38539655+gavrun@users.noreply.github.com>
2024-11-28 22:09:29 +0000

    Added task 5 updated report

---
+++ Lab01/DiaTwoForms/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiaTwoForms
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddPolygon(new Point[]
+                {
+                    new Point(this.Width / 2, 0),
+                    new Point(0, this.Height / 2),
+                    new Point(this.Width / 2, this.Height),
+                    new Point(this.Width, this.Height / 2)
+                }
+            );
+            this.Region = new Region(myPath);
+
+        }
+    }
+}
---
+++ Lab01/ResizeForm/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ResizeForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(35, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Input something";
+            //label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(148, 28);
+            textBox1.MinimumSize = new Size(75, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(195, 25);
+            textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            richTextBox1.Location = new Point(35, 77);
+            richTextBox1.MinimumSize = new Size(150, 150);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(173, 150);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button1.Location = new Point(268, 77);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Send";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(384, 261);
+            Controls.Add(button1);
+            Controls.Add(richTextBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            MinimumSize = new Size(400, 300);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private RichTextBox richTextBox1;
+        private Button button1;
+    }
+}
