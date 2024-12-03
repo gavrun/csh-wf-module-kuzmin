aee40ac5eb5fb64456bb0f667400730f15ffffec
gavrun <38539655+gavrun@users.noreply.github.com>
2024-12-03 14:45:05 +0000

    Added task 4

---
+++ Lab04/NonModalForm/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace NonModalForm
+{
+    public partial class Form1 : Form
+    {
+        public string SolData
+        {
+            get
+            { return textBox1.Text; }
+            set
+            { textBox1.Text = value; }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.Show(this);
+        }
+    }
+}
