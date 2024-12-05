d8b2567982bf11b691e83166615687484c462712
gavrun <38539655+gavrun@users.noreply.github.com>
2024-12-05 21:43:55 +0000

    Added task 3 and report

---
+++ Lab07/AsyncMethod/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace AsyncMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelA = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.buttonWork = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.Location = new System.Drawing.Point(21, 21);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(44, 13);
+            this.labelA.TabIndex = 0;
+            this.labelA.Text = "Value A";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.Location = new System.Drawing.Point(198, 25);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(44, 13);
+            this.labelB.TabIndex = 1;
+            this.labelB.Text = "Value B";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(71, 18);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA.TabIndex = 2;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(248, 22);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxB.TabIndex = 3;
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(24, 61);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 4;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // buttonWork
+            // 
+            this.buttonWork.Location = new System.Drawing.Point(167, 115);
+            this.buttonWork.Name = "buttonWork";
+            this.buttonWork.Size = new System.Drawing.Size(75, 23);
+            this.buttonWork.TabIndex = 5;
+            this.buttonWork.Text = "Work";
+            this.buttonWork.UseVisualStyleBackColor = true;
+            this.buttonWork.Click += new System.EventHandler(this.buttonWork_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(136, 66);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 13);
+            this.labelResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 161);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonWork);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.labelA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Button buttonWork;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ Lab07/AsyncMethod/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncMethod
+{
+    public partial class Form1 : Form
+    {
+        private delegate int AsyncSumma(int a, int b);
+
+        delegate void PrintLabel(string str);
+
+        private PrintLabel PrintDlegateFunc;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            PrintDlegateFunc = new PrintLabel(PrintFunc);
+        }
+
+
+        private int Summa(int a, int b)
+        {
+            System.Threading.Thread.Sleep(7000);
+
+            return a + b;
+        }
+
+        private void CallBackMethod(IAsyncResult ar)
+        {
+            string str;
+            AsyncSumma summaDelegate = (AsyncSumma)ar.AsyncState;
+
+            str = String.Format("Sum of values equals {0}", summaDelegate.EndInvoke(ar));
+
+            MessageBox.Show(str, "Operation result");
+
+            labelResult.Invoke(PrintDlegateFunc, new object[] { str });
+        }
+
+        void PrintFunc(string str)
+        {
+            labelResult.Text = str;
+        }
+
+
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            int a, b;
+
+            //if (!int.TryParse(textBoxA.Text, out a) || !int.TryParse(textBoxB.Text, out b))
+            //{
+            //    MessageBox.Show("Only integers");
+            //    return;
+            //}
+
+            try
+            {
+                a = Int32.Parse(textBoxA.Text);
+                b = Int32.Parse(textBoxB.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+                //throw;
+                return;
+            }
+
+            AsyncSumma asyncSummaDelegate = new AsyncSumma(Summa);
+
+            AsyncCallback cb = new AsyncCallback(CallBackMethod);
+
+            asyncSummaDelegate.BeginInvoke(a, b, cb, asyncSummaDelegate);
+        }
+
+        private void buttonWork_Click(object sender, EventArgs e)
+        {
+            string str = "Task completed";
+
+            MessageBox.Show(str, "Task");
+        }
+    }
+}
