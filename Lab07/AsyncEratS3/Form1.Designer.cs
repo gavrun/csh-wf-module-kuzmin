47032e4e02f33f0af89e9e463ffd26ae573f5e3a
gavrun <38539655+gavrun@users.noreply.github.com>
2024-12-06 16:59:00 +0000

    Add task 5 refactor and report

---
+++ Lab07/AsyncEratS3/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace AsyncEratS2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelMaxValue = new System.Windows.Forms.Label();
+            this.textBoxMaxValue = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.richTextBoxResult = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // labelMaxValue
+            // 
+            this.labelMaxValue.AutoSize = true;
+            this.labelMaxValue.Location = new System.Drawing.Point(37, 34);
+            this.labelMaxValue.Name = "labelMaxValue";
+            this.labelMaxValue.Size = new System.Drawing.Size(87, 13);
+            this.labelMaxValue.TabIndex = 0;
+            this.labelMaxValue.Text = "Input Max Value:";
+            // 
+            // textBoxMaxValue
+            // 
+            this.textBoxMaxValue.Location = new System.Drawing.Point(130, 31);
+            this.textBoxMaxValue.Name = "textBoxMaxValue";
+            this.textBoxMaxValue.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMaxValue.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(259, 31);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalc.TabIndex = 3;
+            this.buttonCalc.Text = "Calc";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // richTextBoxResult
+            // 
+            this.richTextBoxResult.Location = new System.Drawing.Point(40, 80);
+            this.richTextBoxResult.Name = "richTextBoxResult";
+            this.richTextBoxResult.Size = new System.Drawing.Size(294, 322);
+            this.richTextBoxResult.TabIndex = 4;
+            this.richTextBoxResult.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 450);
+            this.Controls.Add(this.richTextBoxResult);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.textBoxMaxValue);
+            this.Controls.Add(this.labelMaxValue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMaxValue;
+        private System.Windows.Forms.TextBox textBoxMaxValue;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.RichTextBox richTextBoxResult;
+    }
+}
+
---
+++ Lab07/AsyncEratS3/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncEratS2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GoButt()
+        {
+            // set max value and create stringbuilder to collect results
+            int maxValue = 0;
+            StringBuilder resultText = new StringBuilder();
+
+            if (int.TryParse(textBoxMaxValue.Text, out maxValue)) //verify if conversion worked
+            {
+                // use array to find prime numbers
+                bool[] isPrime = new bool[maxValue + 1]; // maxValue + 1 human readable match to a number
+
+                // initially populate with pseudo-prime numbers
+                for (int i = 2; i <= maxValue; i++)
+                {
+                    isPrime[i] = true; // assume that all numbers are prime
+                }
+
+                // sieve of eratosthenes
+                for (int p = 2; p * p <= maxValue; p++)
+                {
+                    if (isPrime[p]) // take marked number
+                    {
+                        // find compound numbers by condition
+                        for (int i = p * p; i <= maxValue; i += p) 
+                        {
+                            isPrime[i] = false; // and unmark it 
+                        }
+                    }
+                }
+
+                //push results from array to string builder
+                for (int i = 2; i <= maxValue; i++)
+                {
+                    if (isPrime[i])
+                    {
+                        resultText.AppendFormat("{0} ", i);
+                    }
+                }
+            }
+            else
+            {
+                resultText.Append("Error: Input integer");
+            }
+
+            return resultText.ToString();
+        }
+
+        public string GoButtParallel()
+        {
+            int maxValue = 0;
+            int primeCount = 0; // for large values
+
+            StringBuilder resultText = new StringBuilder();
+
+            if (int.TryParse(textBoxMaxValue.Text, out maxValue))
+            {
+                bool[] isPrime = new bool[maxValue + 1];
+
+                for (int i = 2; i <= maxValue; i++)
+                {
+                    isPrime[i] = true;
+                }
+
+                // use Parallel cycle to process numbers separately
+                Parallel.For(
+                        2, 
+                        (int)Math.Sqrt(maxValue) + 1, // range between 2 and sqrt(max) + 1
+                        p =>
+                        {
+                            if (isPrime[p])
+                            {
+                                for (int i = p * p; i <= maxValue; i += p)
+                                {
+                                    isPrime[i] = false;
+                                }
+                            }
+                        }
+                        );
+
+                for (int i = 2; i <= maxValue; i++)
+                {
+                    if (isPrime[i])
+                    {
+                        primeCount++; // let's count
+
+                        resultText.AppendFormat("{0} ", i);
+                    }
+                }
+            }
+            else
+            {
+                resultText.Append("Error: Input integer");
+            }
+
+            //return resultText.ToString();
+
+            if (primeCount < 10000000)
+            {
+                return $"Found {primeCount} primes.\n\n" + resultText.ToString();
+            }
+            return $"Found {primeCount} primes.";
+        }
+
+        private async void buttonCalc_Click(object sender, EventArgs e)
+        {
+            richTextBoxResult.Text = "Calculating...";
+
+            //string result = await Task.Run(() => GoButt());
+            string result = await Task.Run(() => GoButtParallel());
+
+            richTextBoxResult.Text = result;
+        }
+
+    }
+}
